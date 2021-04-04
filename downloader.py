49b4c7031b0c0a30e3a2c5e010e8c1d4777dd0c5
i207M <connect@mail.i207m.top>
2021-04-05 01:45:29 +0800

    ok

--- downloader.py
+++ downloader.py
@@ -8,15 +8,18 @@ __author__ = 'i207M'
 # URL_cookie_example = 'http://reserves.lib.tsinghua.edu.cn/books/00000398/00000398000/index.html'
 
 
-def url_available(url: str) -> bool:
-    # print(url)
-    res = requests.get(url)
-    return res.status_code == 200
-
-
 def mkdir(path: str) -> None:
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+cookie = {}
+
+
+def url_available(url: str) -> bool:
+    # print(url)
+    res = requests.get(url, cookies=cookie)
+    return res.status_code == 200
 
 
 def url_shape(url: str) -> str:
@@ -39,14 +42,16 @@ def claw(url: str) -> None:
     mkdir(path)
 
     # read cookies
-    need_cookie = ('//' in url)
-    cookie = {}
+    need_cookie = ('//' not in url)
     if need_cookie:
+        if not os.path.exists('cookie.txt'):
+            raise Exception('No cookie data. See README.md for help.')
         with open('cookie.txt') as f:
             _data = [v.strip() for v in f.read().splitlines()]
             if len(_data) != 2:
                 raise Exception('Cookie data error')
-            cookie['ASPXAUTH'] = _data[0]
+            global cookie
+            cookie['.ASPXAUTH'] = _data[0]
             cookie['ASP.NET_SessionId'] = _data[1]
 
     # claw
@@ -58,14 +63,14 @@ def claw(url: str) -> None:
         # print(image_url)
         cnt = 0
         while True:
-            res = requests.get(image_url.format(cnt), cookies=cookie)
+            res = requests.get(image_url.format(cnt + 1), cookies=cookie)
             if res.status_code != 200:
                 if res.status_code == 404:
                     print(f'Clawed: {id=}, {cnt=}')
                     break
                 raise Exception('HTTP error')
             if len(res.content) < 10 * 1024:
-                raise Exception('Unable to download, perhaps due to invalid cookie')
+                raise Exception('Unable to download. Perhaps due to invalid cookie.')
             with open(f'{path}/{page_num:05d}.jpg', 'wb+') as f:
                 f.write(res.content)
             cnt += 1
