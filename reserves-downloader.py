47447e33c6d369eb62236b496614a99a79521349
i207M <connect@mail.i207m.top>
2021-04-05 00:35:49 +0800

    book

--- reserves-downloader.py
+++ reserves-downloader.py
@@ -35,26 +35,30 @@ def claw(url: str) -> None:
     if not url.endswith('index.html'):
         raise Exception('URL Invalid!')
     url = url_shape(url)
+    index_url = 'http://' + url + '{:03d}/index.html'
+    image_url_base = 'http://' + url.replace('//', '/')
     path = './clawed_' + url[url.rfind('/') + 1:]
     mkdir(path)
+
     id = 0
     page_num = 0
-    index_url = url + '{:03d}/index.html'
     print('Start clawing...')
-    while id <= 999 and url_available('http://' + index_url.format(id)):
-        image_url = 'http://' + url.replace('//', '/') + f'{id:03d}/files/mobile/{{}}.jpg'
+    while id <= 999 and url_available(index_url.format(id)):
+        image_url = image_url_base + f'{id:03d}/files/mobile/{{}}.jpg'
         # print(image_url)
         cnt = 1
         while cnt != 0:
             try:
                 request.urlretrieve(image_url.format(cnt), f'{path}/{page_num:05d}.jpg')
-                page_num = page_num + 1
+                page_num += 1
+                print(image_url.format(cnt))
+                exit()
             except urllib.error.HTTPError as e:
                 assert e.code == 404
                 print(f'Clawed: {id=}, {cnt=}')
                 cnt = -1
-            cnt = cnt + 1
-        id = id + 1
+            cnt += 1
+        id += 1
     print(f'Total page number: {page_num}')
 
 
