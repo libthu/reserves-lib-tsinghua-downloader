ff36e04cc7dae788535f79553d8b8563c4bdb742
i207M <connect@mail.i207m.top>
2021-04-05 20:35:20 +0800

    code improvement and created cookie.txt

--- downloader.py
+++ downloader.py
@@ -5,7 +5,7 @@ __author__ = 'i207M'
 
 # URL_example = 'http://reserves.lib.tsinghua.edu.cn/book4//00013082/00013082000/index.html'
 # URL_image_example = 'http://reserves.lib.tsinghua.edu.cn/book4/00013082/00013082000/files/mobile/1.jpg'
-# URL_cookie_example = 'http://reserves.lib.tsinghua.edu.cn/books/00000398/00000398000/index.html'
+# URL_need_cookie_example = 'http://reserves.lib.tsinghua.edu.cn/books/00000398/00000398000/index.html'
 
 
 def mkdir(path: str) -> None:
@@ -18,18 +18,32 @@ cookie = {}
 
 def url_available(url: str) -> bool:
     # print(url)
-    res = requests.get(url, cookies=cookie)
-    return res.status_code == 200
+    ret = requests.get(url, cookies=cookie)
+    return ret.status_code == 200
 
 
 def url_shape(url: str) -> str:
-    if not (url.startswith('http://') and url.endswith('index.html')):
+    if not (url.startswith('http://reserves.lib.tsinghua.edu.cn/') and url.endswith('index.html')):
         raise Exception('Invalid URL')
     url = url[7:-11]  # 'http://', '/index.html'
     if url.endswith('mobile'):
         url = url[:-7]
     url = url[:-3]  # '000'
     return url
+
+
+def cookie_init():
+    COOKIE_PATH = 'cookie.txt'
+
+    if not os.path.exists(COOKIE_PATH):
+        raise Exception(f'No cookie data in "{COOKIE_PATH}". See README.md for help.')
+    with open(COOKIE_PATH) as f:
+        _data = [v.strip() for v in f.read().splitlines()]
+        if len(_data) != 2:
+            raise Exception('Cookie data error')
+        global cookie
+        cookie['.ASPXAUTH'] = _data[0]
+        cookie['ASP.NET_SessionId'] = _data[1]
 
 
 def claw(url: str) -> None:
@@ -41,18 +55,10 @@ def claw(url: str) -> None:
     path = './clawed_' + url[url.rfind('/') + 1:]
     mkdir(path)
 
-    # read cookies
+    # process cookies
     need_cookie = ('//' not in url)
     if need_cookie:
-        if not os.path.exists('cookie.txt'):
-            raise Exception('No cookie data. See README.md for help.')
-        with open('cookie.txt') as f:
-            _data = [v.strip() for v in f.read().splitlines()]
-            if len(_data) != 2:
-                raise Exception('Cookie data error')
-            global cookie
-            cookie['.ASPXAUTH'] = _data[0]
-            cookie['ASP.NET_SessionId'] = _data[1]
+        cookie_init()
 
     # claw
     id = 0
@@ -63,16 +69,17 @@ def claw(url: str) -> None:
         # print(image_url)
         cnt = 0
         while True:
-            res = requests.get(image_url.format(cnt + 1), cookies=cookie)
-            if res.status_code != 200:
-                if res.status_code == 404:
+            ret = requests.get(image_url.format(cnt + 1), cookies=cookie)
+            if ret.status_code != 200:
+                if ret.status_code == 404:
                     print(f'Clawed: {id=}, {cnt=}')
                     break
                 raise Exception('HTTP error')
-            if len(res.content) < 10 * 1024:
-                raise Exception('Unable to download. Perhaps due to invalid cookie.')
+            # A login html (~7kB) is downloaded when cookies are invalid.
+            if need_cookie and len(ret.content) < 10 * 1024:
+                raise Exception('Unable to download. Perhaps due to invalid cookies.')
             with open(f'{path}/{page_num:05d}.jpg', 'wb+') as f:
-                f.write(res.content)
+                f.write(ret.content)
             cnt += 1
             page_num += 1
         id += 1
