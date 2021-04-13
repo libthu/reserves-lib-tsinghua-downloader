cd9a488b55186a1332476d6fdd6d0621456b9560
i207M <connect@mail.i207m.top>
2021-04-13 16:59:29 +0800

    generate pdf

--- test_downloader.py
+++ test_downloader.py
@@ -3,3 +3,7 @@ from downloader import claw
 
 def test_claw_default():
     claw('http://reserves.lib.tsinghua.edu.cn/book5//00001471/00001471000/index.html')
+
+
+if __name__ == '__main__':
+    test_claw_default()
