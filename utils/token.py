2251da828557b8e5cb433c702449cbc2fab4b6dd
Yasar James <3160536901@qq.com>
2024-09-13 00:37:54 +0800

    feature: input and save token when running without token.txt

--- utils/token.py
+++ utils/token.py
@@ -6,16 +6,19 @@ import requests
 def get_token():
     TOKEN_PATH = "token.txt"
     if not os.path.exists(TOKEN_PATH):
-        print("Cookie Required.")
+        print("Token Required.")
         print("See README.md for help.")
         print("*" * 30)
-        raise FileNotFoundError(f'No such file: "{TOKEN_PATH}"')
-    with open(TOKEN_PATH) as f:
-        data = [v.strip() for v in f.read().splitlines()]
-    if len(data) != 1:
-        raise Exception(f'Too many or too many lines in "{TOKEN_PATH}"')
-
-    token = data[0]
+        token = input("Please input your token: ")
+        with open(TOKEN_PATH, "w") as f:
+            f.write(token)
+        # raise FileNotFoundError(f'No such file: "{TOKEN_PATH}"')
+    else:
+        with open(TOKEN_PATH) as f:
+            data = [v.strip() for v in f.read().splitlines()]
+        if len(data) != 1:
+            raise Exception(f'Too many or too many lines in "{TOKEN_PATH}"')
+        token = data[0]
     return token
 
 
